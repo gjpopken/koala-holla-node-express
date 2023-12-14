1a499126f49c6f6d256ff9688c77b32e4938216a
slindsey9150 <stevie.lindsey.jr@gmail.com>
2023-12-14 17:21:25 -0600

    get route fully working

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -7,18 +7,33 @@ const pool = require('../modules/pool')
 
 
 // GET
-koalaRouter.get('/', (req,res) => {
+koalaRouter.get('/', (req, res) => {
     console.log('GET route working')
+    let queryText = `
+    SELECT * FROM "koalas"
+    `
+    pool.query(queryText).then((result) => {
+        res.send(result.rows)
+    })
+        .catch((error) => {
+            console.log("error getting books", error);
+            res.sendStatus(500)
 
+        })
 })
+
 // POST
-koalaRouter.post('/', (req,res) =>{
+koalaRouter.post('/', (req, res) => {
     console.log("POST route working")
 })
 
 // PUT
-koalaRouter.put('/', (req,res) =>{
+koalaRouter.put('/', (req, res) => {
     console.log("PUT route working")
+
+    pool.query = `
+    SELECT * FROM "koalas"
+    `
 })
 
 // DELETE
