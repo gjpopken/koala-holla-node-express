57d560a82635bf9c96612ba0b847be2e1b461fe9
Gavin Popken <gavinpopkenart@gmail.com>
2023-12-14 15:49:07 -0600

    working on client get function

---
+++ database.sql
@@ -0,0 +1,29 @@
+CREATE TABLE "koalas"(
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80) NOT NULL,
+	"gender" VARCHAR(80),
+	"age" INTEGER,
+	"ready_to_transfer" BOOLEAN DEFAULT FALSE,
+	"comments" VARCHAR(500)
+	);
+	
+INSERT INTO "koalas"
+	("name", "gender", "age", "ready_to_transfer", "comments")
+	VALUES
+	('Scotty', 'M', 4, true, 'Born in Guatemala'),
+	('Jean', 'F', 5, true, 'Allergic to lots of lava'),
+	('Ororo', 'F', 7, false, 'Loves listening to Paula (Abdul)'),
+	('K''leaf', 'NB', 15, false, 'Never refuses a treat'),
+	('Charlie', 'M', 9, true, 'Favorite band is Nirvana'),
+	('Betsy', 'F', 4, true, 'Has a pet iguana');
+DELETE FROM "koalas"
+	WHERE "id" = $1;
+UPDATE "koalas"
+	SET "ready_to_transfer" = TRUE
+	WHERE "id" = $1;
+SELECT * FROM "koalas"
+    ORDER BY "id";
+INSERT INTO "koalas"
+	("name", "gender", "age", "ready_to_transfer", "comments")
+	VALUES
+	($1, $2, $3, $4, $5);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,110 @@
 console.log( 'js' );
 
+let hardCodedKoalas = [
+  {
+    name: 'Kevin',
+    age: 7,
+    gender: 'male',
+    readyToTransfer: false,
+    comments: 'is cool'
+  },
+  {
+    name: 'James',
+    age: 4,
+    gender: 'male',
+    readyToTransfer: false,
+    comments: 'is dumb'
+  },
+  {
+    name: 'Jane',
+    age: 6,
+    gender: 'female',
+    readyToTransfer: true,
+    comments: 'is also cool'
+  }
+]
+
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
+  const tbody = document.getElementById('viewKoalas')
+  tbody.innerHTML = ''
+  for (koala of hardCodedKoalas) { // TODO change to the response data
+    let newInnerHTML = `
+    <tr>
+      <td>${koala.name}</td>
+      <td>${koala.age}</td>
+      <td>${koala.gender}</td>
+      <td>${koala.comments}</td>
+      <td>${koala.readyToTransfer}</td>
+    `
+    if (koala.readyToTransfer === false) {
+      newInnerHTML += `
+          <td>
+            <button>Mark as Ready</button>
+          </td></tr>
+          `
+    } else {
+
+      newInnerHTML += `
+        </tr>
+      `
+    }
+    console.log(newInnerHTML);
+
+
+    
+  //   tbody.innerHTML += `
+  //   <tr>
+  //     <td>${koala.name}</td>
+  //     <td>${koala.age}</td>
+  //     <td>${koala.gender}</td>
+  //     <td>${koala.comments}</td>
+  //     <td>${koala.readyToTransfer}</td>
+  //   `
+  //   if (koala.readyToTransfer === false) {
+  //     console.log('current innerHTML of tbody', tbody.innerHTML);
+  //     tbody.innerHTML += `
+  //     <td>
+  //       <button>Mark as Ready</button>
+  //     </td></tr>
+  //     `
+  //   } else {
+  //     tbody.innerHTML += `
+  //       </tr>
+  //     `
+  //   }
+  // }
+
+  // axios({
+  //   method: 'GET',
+  //   url: '/koalas'
+  // }).then((response) => {
+  //   let koalas = response.data
+  //   const tbody = document.getElementById('viewKoalas')
+  //   tbody.innerHTML = ''
+  //   for (koala of hardCodedKoalas) { // TODO change to the response data
+  //     tbody.innerHTML += `
+  //     <tr>
+  //       <td>${koala.name}</td>
+  //       <td>${koala.age}</td>
+  //       <td>${koala.gender}</td>
+  //       <td>${koala.comments}</td>
+  //       <td>${koala.readyToTransfer}</td>
+  //     `
+  //     if (koala.readyToTransfer === false) {
+  //       tbody.innerHTML += `
+  //       <td>
+  //         <button>Mark as Ready</button>
+  //       </td></tr>
+  //       `
+  //     } else {
+  //       tbody.innerHTML += `
+  //         </tr>
+  //       `
+  //     }
+  //   }
+  // }) // end of .then
   
 } // end getKoalas
 
