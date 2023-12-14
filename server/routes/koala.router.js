e658cb54d02482420483843f3529dbd27d4c0259
slindsey9150 <stevie.lindsey.jr@gmail.com>
2023-12-14 16:41:03 -0600

    created pool and database, and we are currently trying to get a GET route to work with postman

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,8 +5,15 @@ const koalaRouter = express.Router();
 
 
 // GET
-
-
+koalaRouter.get('/koalas', (req,res) => {
+    console.log('GET route working')
+  res.sendStatus(200)
+    
+    .catch((error) => {
+        console.log('WE HIT THE KOALA WITH OUR SUBARU OUTBACK', error)
+        res.sendStatus(500)
+    })
+})
 // POST
 
 
