a5a196d1095bd54f94a7868335116a6eeb84914d
slindsey9150 <stevie.lindsey.jr@gmail.com>
2023-12-14 18:16:42 -0600

    able to update database with a predetermined value based on predetermined id

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -10,7 +10,7 @@ const pool = require('../modules/pool')
 koalaRouter.get('/', (req, res) => {
     console.log('GET route working')
     let queryText = `
-    SELECT * FROM "koalas;"
+    SELECT * FROM "koalas";
     `
     pool.query(queryText).then((result) => {
         res.send(result.rows)
@@ -26,7 +26,7 @@ koalaRouter.get('/', (req, res) => {
 koalaRouter.post('/', (req, res) => {
     console.log("POST route working")
 
-// ! this section deals with SQL injection by establishinng queryParams instead of pure SQL being sent to the database
+    // ! this section deals with SQL injection by establishinng queryParams instead of pure SQL being sent to the database
     let koala = req.body
     console.log('this is koala', koala);
     let queryText = `
@@ -42,9 +42,9 @@ INSERT INTO "koalas"
             console.log('oops we hit an error in the POST query')
             res.sendStatus(404)
         })
-// end of section dealing with SQL Injection
+    // end of section dealing with SQL Injection
 
-// ! this section is to test that the post route can update the database with predetermined data
+    // ! this section is to test that the post route can update the database with predetermined data
     // let queryText = `INSERT INTO "koalas"
     // ("name", "gender", "age", "ready_to_transfer", "comments")
     // VALUES
@@ -52,19 +52,23 @@ INSERT INTO "koalas"
     //     `
     // pool.query(queryText).then((result) => {
     //     res.sendStatus(201)
-// end of section inserting predetermined data into the database
+    // end of section inserting predetermined data into the database
 })
 
 // PUT
 koalaRouter.put('/', (req, res) => {
     console.log("PUT route working")
 
-queryText =    `
-    SELECT * FROM "koalas"
+// ! this sectioin is able to update data on the database with a predetermined value and id
+    queryText = `
+    UPDATE "koalas"
+    SET "ready_to_transfer" = TRUE
+    WHERE "id" = 3;
     `
     pool.query(queryText).then((result) => {
-        res.send(result.rows);
+        res.send(result);
     })
+//end of section that can update database with a predetermined value and id
 })
 
 // DELETE
